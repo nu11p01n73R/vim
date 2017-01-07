2b28878fb115c5de58338b5324f9c5451885845f
nu11p01n73R <kjjohny1991@gmail.com>
2017-01-07 23:40:45 +0530

    added vim-go

--- setup.sh
+++ setup.sh
@@ -1,27 +1,27 @@
 #!/usr/local/bin/bash
 
 function createDirectory {
-    if [[ ! -d "$1" ]]
-    then
-        mkdir -p "$HOME/.vim/$1"
-    fi
+        if [[ ! -d "$1" ]]
+        then
+                mkdir -p "$HOME/.vim/$1"
+        fi
 }
 
 function installPathogen {
-    if [[ ! -f "$HOME/.vim/autoload/pathogen.vim" ]]
-    then
-        curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-    fi
+        if [[ ! -f "$HOME/.vim/autoload/pathogen.vim" ]]
+        then
+                curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+        fi
 }
 
 function createVimrc {
-    if [[ ! -f "$HOME/.vimrc" ]]
-    then
-        ln -s "$HOME/.vim/vimrc" "$HOME/.vimrc"
-    fi
+        if [[ ! -f "$HOME/.vimrc" ]]
+        then
+                ln -s "$HOME/.vim/vimrc" "$HOME/.vimrc"
+        fi
 }
 
 createDirectory "autoload" \
-    && createDirectory "bundle" \
-    && installPathogen  \
-    && createVimrc
+        && createDirectory "bundle" \
+        && installPathogen  \
+        && createVimrc
